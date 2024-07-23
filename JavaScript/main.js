2d4c5e2770a4549b42f4a37ce0d2fed99e28103b
Diego Fischer <diegof01gm@gmail.com>
2024-07-22 21:06:01 -0300

    Formas de cargar los productos desde JS

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,24 +1,24 @@
 // /*JS TP Final con Jose*/ 
 
-// //Cargar arrays desde JS.
+//Cargar arrays desde JS.
 
-// let lista = document.getElementById("lista");
-// const productos = ["Pan", "Leche", "Carne"];
-// const precios = ["1000", "2000", "3000"];
+let lista = document.getElementById("productos");
+const productos = ["Pan", "Leche", "Carne" , "Yerba", "Fideos", "Arroz"];
+const precios = ["1000", "2000", "3000", "4000" , "5000","6000"];
 
-// function pintarProductos(arrayProductos, arrayPrecios) {
+function cargarProductos(arrayProductos, arrayPrecios) {
 
-//     for (let i = 0; i < arrayProductos.length; i++) {
-//         let li = document.createElement("li");
-//         let texto = document.createTextNode(`Nombre: ${arrayProductos[i]} - Precio: $${arrayPrecios[i]}`);
-//         li.appendChild(texto);
-//         li.setAttribute("class", "nombreDeLaClase")
-//         lista.appendChild(li);
-//     }
+    for (let i = 0; i < arrayProductos.length; i++) {
+        let li = document.createElement("li");
+        let texto = document.createTextNode(`${arrayProductos[i]} - Precio: $${arrayPrecios[i]}`);
+        li.appendChild(texto);
+        li.setAttribute("class", "nombreDeLaClase")
+        lista.appendChild(li);
+    }
 
-// }
+}
 
-// pintarProductos(productos,precios);
+cargarProductos(productos,precios);
 // //Despues si quiero agregar mas, los agrego en los arrays de Productos y Precios; pero NO modifico la funcion.
 
 
@@ -50,3 +50,17 @@
 // crearImagen("./imagenJS.png" , "Imagen de JS");
 
 // crearImagen("./flores.jpg" , "Flores");
+
+
+//-----------------------------------------------------------------------------------------------------
+
+// Tambien se pueden cargar con json.
+
+// const producto={
+//     "nombre":"arroz",
+//     "cantidad":35,
+//     "precio":1500
+// }
+
+// console.log("Del producto "+ producto.nombre + " tengo "
+//     + producto.cantidad + " unidades que valen c/u $"+ producto.precio);
