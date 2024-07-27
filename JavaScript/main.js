c21ec84638200951d9f1389d396b6902be597fbf
Diego Fischer <diegof01gm@gmail.com>
2024-07-27 15:54:52 -0300

    Formulario de contacto en .txt

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -44,6 +44,31 @@ function cargarProductos(arrayProductos, arrayPrecios, arrayStock) {
 cargarProductos(productos, precios, stock);
 
 
+//Formulario de contacto:
+let nombre = document.getElementById("nombre");
+let apellido = document.getElementById("apellido");
+let email = document.getElementById("email");
+let btnenviar = document.getElementById("enviar");
+let informacion = [];
+
+btnenviar.addEventListener("click" , (e)=>{
+    e.preventDefault();
+    informacion[0] = nombre.value;
+    informacion[1] = apellido.value;
+    informacion[2] = email.value;
+
+    let blob = new Blob([informacion] , {type: "text/plai:charset=utf-8"});
+    saveAs(blob, "contact.txt");
+})
+
+
+
+
+
+
+
+
+
 
 // //---------------------------------------------------------------------------------------
 
