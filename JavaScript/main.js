87ee63e5bd819080499423e797bb2add7dd8da68
Diego Fischer <diegof01gm@gmail.com>
2024-07-27 16:18:09 -0300

    correcciones

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -47,7 +47,7 @@ cargarProductos(productos, precios, stock);
 //Formulario de contacto:
 let nombre = document.getElementById("nombre");
 let apellido = document.getElementById("apellido");
-let email = document.getElementById("email");
+// let email = document.getElementById("email");
 let btnenviar = document.getElementById("enviar");
 let informacion = [];
 
@@ -55,9 +55,9 @@ btnenviar.addEventListener("click" , (e)=>{
     e.preventDefault();
     informacion[0] = nombre.value;
     informacion[1] = apellido.value;
-    informacion[2] = email.value;
+    // informacion[2] = email.value;
 
-    let blob = new Blob([informacion] , {type: "text/plai:charset=utf-8"});
+    let blob = new Blob([informacion] , {type: "text/plain;charset=utf-8"});
     saveAs(blob, "contact.txt");
 })
 
