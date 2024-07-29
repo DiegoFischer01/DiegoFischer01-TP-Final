550b1091c533e52ec5c7afb0a866c5e3d3aebe7c
Diego Fischer <diegof01gm@gmail.com>
2024-07-29 20:36:21 -0300

    Listo formulario

--- JavaScript/formulario.js
+++ JavaScript/formulario.js
@@ -5,7 +5,9 @@
 //Formulario de contacto:
 let nombre = document.getElementById("nombre");
 let apellido = document.getElementById("apellido");
-// let email = document.getElementById("email");
+let email = document.getElementById("email");
+let telefono = document.getElementById("telefono");
+let consulta= document.getElementById("consulta");
 let btnenviar = document.getElementById("enviar");
 let informacion = [];
 
@@ -13,8 +15,10 @@ btnenviar.addEventListener("click", (e) => {
     e.preventDefault();
     informacion[0] = nombre.value;
     informacion[1] = apellido.value;
-    // informacion[2] = email.value;
-
+    informacion[2] = email.value;
+    informacion[3] = telefono.value;
+    informacion[4] = consulta.value;
+    
     let blob = new Blob([informacion], { type: "text/plain;charset=utf-8" });
     saveAs(blob, "contact.txt");
 })
