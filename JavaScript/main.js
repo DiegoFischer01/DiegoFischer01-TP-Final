c39d6de63d6627240dcb597af8ea819284797bea
Diego Fischer <diegof01gm@gmail.com>
2024-07-30 20:35:11 -0300

    depuracion de codigo

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -65,38 +65,6 @@ function finalizarCompra() {
 }
 
 
-
-//----------------------------------------------------------------------------------------------------
-
-//Formulario de contacto:
-let nombre = document.getElementById("nombre");
-let apellido = document.getElementById("apellido");
-// let email = document.getElementById("email");
-let btnenviar = document.getElementById("enviar");
-let informacion = [];
-
-btnenviar.addEventListener("click", (e) => {
-    e.preventDefault();
-    informacion[0] = nombre.value;
-    informacion[1] = apellido.value;
-    // informacion[2] = email.value;
-
-    let blob = new Blob([informacion], { type: "text/plain;charset=utf-8" });
-    saveAs(blob, "contact.txt");
-})
-
-
-
-
-
-
-
-
-
-
-// //---------------------------------------------------------------------------------------
-
-
 // //Funcion para crear imagenes desde JS.
 
 // function crearImagen(url, textoAlternativo){
